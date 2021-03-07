89790c9bb7ace1e7f7b4b50ac7b22810a5f4e02c
Adam Du <dulinnan@gmail.com>
2021-03-07 21:51:38 +1300

    :bug: fix: using nested list to handle both single line and multi-lined file

--- compsci130/count_word_occurances.py
+++ compsci130/count_word_occurances.py
@@ -16,15 +16,30 @@ def prompt_user_input_for_filename():
     filename = input("Enter filename: ")
     return filename
 
+def handle_read_file(read_lines_list): 
+    nested_list = []
+    output = []
+    for line in read_lines_list:
+        read_words_list = line.replace("\n", ",").strip()
+        read_words_list = read_words_list.replace(" ", ",").strip()
+        read_words_list = read_words_list.split(",")
+        nested_list.append(read_words_list)
+    while nested_list:
+        # bad way of flattening a nested list 
+        # because .pop() is O(n)
+        # but there is no way to do it in linear time 
+        # from a list in Python without extra storage space
+        output.extend(nested_list.pop(0))
+    return output
+    
 def count_word_occurances():
     path_to_file = prompt_user_input_for_filename() 
     with open(path_to_file) as f:
-        read_words_list = f.read()
-        read_words_list = read_words_list.replace("\n", ",").strip()
-        read_words_list = read_words_list.replace(" ", ",").strip()
-        read_words_list = read_words_list.split(",")
+        read_words_list = f.readlines()
     f.close()
-    sanitised_list = sanitise_list(read_words_list)
+    words_list = handle_read_file(read_words_list)
+    sanitised_list = sanitise_list(words_list)
+    print(sanitised_list)
 
 
 if __name__ == "__main__":
